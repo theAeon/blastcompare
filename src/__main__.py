d6ff76eea7b11acb8966f5b1eab78f7fc4530223
Andrew Robbins <andrew@robbinsa.me>
2022-04-19 15:23:53 -0400

    a bit of linting

--- src/__main__.py
+++ src/__main__.py
@@ -1,42 +1,53 @@
 """ Takes fasta sequences from two ordered fasta files, in which each pair is a known alignment.
-Developed against Python 3.9.7, BioPython 1.78, and NumPy 1.19.5
-Tested with Muscle v3.8.1551.
 """
-import plac
-#from argparse import ArgumentParser
 from collections import Counter
 from io import StringIO
 from os import get_terminal_size
 from pprint import pprint
-from matplotlib.pyplot import show
+from typing import cast, Iterable, Literal, NamedTuple
 
-#from typing import Literal, Tuple
-#from types import new_class
 from Bio import AlignIO, SeqIO, SeqUtils
+from Bio.Data import IUPACData
+
+from matplotlib.pyplot import show
+from Bio.Align import MultipleSeqAlignment
+
+import plac
+
 from skbio import Protein
+
 from skbio_parasail import SubstitutionMatrix
 from skbio_parasail import global_pairwise_align_protein as gpap
 
 import calculate_cons_for_clustal_protein as cons
 import render
 
+clust = Literal[' ', '', '.', ':', '*']
+AminoAcid = Literal['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 
+                    'N', 'P', 'Q', 'R', 'S', 'T', 'W', 'Y', '-', 'X'] 
 
-#AminoAcid = Literal["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y", "-"]
-#Clustal = Literal['','.',':','*']
-#SeqClass=new_class(SeqRecord)
-#Position = Tuple[int, Clustal, AminoAcid, AminoAcid, SeqClass, SeqClass]
-#PairDiff = list[Position]
-def format_print(input1: dict, input2: dict, header1: str, header2: str, modestring: str):
+
+class BasePair(NamedTuple):
+    pos: int
+    clust: clust
+    amino1: AminoAcid
+    amino2: AminoAcid
+    name1: str
+    name2: str
+
+
+def format_print(input1: dict, input2: dict, header1: str, header2: str, modestring: str) -> None:
     print(modestring)
     print(header1)
     pprint(input1)
     print(header2)
     pprint(input2)
-    print(u'\u2500' * term_size)
+    print('\u2500' * term_size)
+
 
 def transform(p_list, pos: int) -> dict:
-    test_a=Counter(list(list(zip(*p_list))[pos]))
-    three_counter={}
+    test_a = Counter(list(list(zip(*p_list))[pos]))
+    three_counter = {}
     for key in test_a:
         three_counter[SeqUtils.seq3(key)] = test_a[key]
     if 'Xaa' in three_counter:
@@ -45,12 +56,18 @@ def transform(p_list, pos: int) -> dict:
 
 
 class SpeciesCompare():
-    def __init__(self,base: str) -> None:
-        self.seq_ids=SeqIO.parse("%sbestIDs.fsa"  % (base), "fasta")
-        self.seqnames=SeqIO.parse("%sbestnames.fsa"  % (base), "fasta")
-        self.seqidlist=list(self.seq_ids)
-        self.seqnamelist=list(self.seqnames)
-    def getpairlist(self, i: int):
+    seq_ids: Iterable[SeqIO.SeqRecord]
+    seqnames: Iterable[SeqIO.SeqRecord]
+    seqidlist: list[SeqIO.SeqRecord]
+    seqnamelist: list[SeqIO.SeqRecord]
+    
+    def __init__(self, base: str) -> None:
+        self.seq_ids = SeqIO.parse("%sbestIDs.fsa" % (base), "fasta")
+        self.seqnames = SeqIO.parse("%sbestnames.fsa" % (base), "fasta")
+        self.seqidlist = list(self.seq_ids)
+        self.seqnamelist = list(self.seqnames)
+
+    def getpairlist(self, i: int) -> list[BasePair]:
         seqnameprot = Protein(str(self.seqnamelist[i].seq))
         seqidprot = Protein(str(self.seqidlist[i].seq))
         alignment = gpap(
@@ -59,106 +76,128 @@ class SpeciesCompare():
         handler = StringIO()
         _ = msa.write(handler, format='clustal')
         clustal = cons.calculate_cons_for_clustal_protein(_.getvalue())
-
-        alignment = AlignIO.read(StringIO(clustal.getvalue()), "clustal")
+        alignmenta = AlignIO.read(StringIO(clustal.getvalue()), "clustal")
+        assert isinstance(alignmenta, MultipleSeqAlignment)
+        alignment = cast(MultipleSeqAlignment, alignmenta)
         pairlist = []
         for kndex, letter in enumerate(alignment.column_annotations['clustal_consensus']):
-            name1=self.seqnamelist[i].description
-            name2=self.seqidlist[i].description
-            base1 = alignment[0, kndex]
-            base2 = alignment[1, kndex]
-            diff = letter
+            name1a = self.seqnamelist[i].description
+            name2a = self.seqidlist[i].description
+            base1a = alignment[0, kndex]
+            base2a = alignment[1, kndex]
+            diffa = letter
             loc = kndex
-            pairlist.append((loc, diff, base1, base2, name1, name2))
+            try:
+                assert diffa in [' ', '', '.', ':', '*'], diffa
+                diff = cast(clust, diffa)
+                assert base1a in IUPACData.protein_letters + '-' + 'X', base1a
+                base1 = cast(AminoAcid, base1a)
+                assert base2a in IUPACData.protein_letters + '-' + 'X', base2a
+                base2 = cast(AminoAcid, base2a)
+                name1 = cast(str, name1a)
+                name2 = cast(str, name2a)
+            except AssertionError as a:
+                print(a)
+                raise
+            else:
+                tup = cast(BasePair, (loc, diff, base1, base2, name1, name2))
+                pairlist.append(tup)
         return pairlist
 
-def compared(input, same):
-    local = SpeciesCompare(input)
-    testAll=[local.getpairlist(i-1) for i in range(len(local.seqidlist)+1)]
+
+def compared(inputa, same):
+    local = SpeciesCompare(inputa)
+    testAll = [local.getpairlist(i-1) for i in range(len(local.seqidlist)+1)]
     if same:
-        flat=[item for sublist in testAll for item in sublist if item[1] == '*']
+        flat = [item for sublist in testAll for item in sublist if item[1] == '*']
     else:
         flat = [item for sublist in testAll for item in sublist if item[1] != '*']
 
-    dictA=transform(flat,2)
-    dictB=transform(flat,3)
-    filename=input
-    #filename=comparator.rsplit('/',1)[1]
-    headerA = filename.split('v')[0] ##this is imprecise and probably a bug waiting to explode
+    dictA = transform(flat, 2)
+    dictB = transform(flat, 3)
+    filename = inputa
+    # filename=comparator.rsplit('/',1)[1]
+    headerA = filename.split('v')[0]  # this is imprecise and probably a bug waiting to explode
     headerB = filename.split('v')[1]
     format_print(dictA, dictB, headerA, headerB, "Printing amino-acids for all proteins combined:")
     render.renderCount(dictA, dictB, headerA, headerB)
 
-def per(input, index, all, residuals):
-        index = int(index)
-        local = SpeciesCompare(input)
-        REMAINING = 0
-        parseList = []
-        renderList = []
-        if all:
-            REMAINING = len(local.seqidlist) - 1
-            while REMAINING >=1:
-                parseList.append(local.getpairlist(REMAINING))
-                REMAINING -= 1
-        else:
-            parseList.append(local.getpairlist(index-1))
-        for parse in parseList:
-            if parse != []:
-                transA=transform(parse, 2)
-                transB=transform(parse, 3)
-                headA=parse[0][4]
-                headB=parse[0][5]
-                if residuals:
-                    if all:
-                        
-                        netDict = {
-                            k: (v - transB[k] if k in transB.keys() else v) for k, v in transA.items()}
-                        renderList.append((netDict, headA + '\n' + headB))
-                        
-                    else:
-                        netDict = {k:(v - transB[k] if k in transB.keys() else v) for k, v in transA.items()}
-                        render.renderSingle(netDict, headA + '\n' + headB)
-                        show()
+
+def per(inputa, index, all_, residuals):
+    index = int(index)
+    local = SpeciesCompare(inputa)
+    REMAINING = 0
+    parseList = []
+    renderList = []
+    if all_:
+        REMAINING = len(local.seqidlist) - 1
+        while REMAINING >= 1:
+            parseList.append(local.getpairlist(REMAINING))
+            REMAINING -= 1
+    else:
+        parseList.append(local.getpairlist(index-1))
+    for parse in parseList:
+        if parse != []:
+            transA = transform(parse, 2)
+            transB = transform(parse, 3)
+            headA = parse[0][4]
+            headB = parse[0][5]
+            if residuals:
+                if all_:
+
+                    netDict = {
+                        k: (v - transB[k] if k in transB.keys() else v) for k, v in transA.items()}
+                    renderList.append((netDict, headA + '\n' + headB))
 
                 else:
-                    format_print(transA, transB, headA, headB, "")
-                    render.renderCount(transA, transB, str(headA), str(headB))
-        if renderList != []:
-            figIns = render.heatmapper(renderList)
-            figIns.rendermulti()
-            show()
+                    netDict = {k: (v - transB[k] if k in transB.keys() else v) for k, v in transA.items()}
+                    render.renderSingle(netDict, headA + '\n' + headB)
+                    show()
+            else:
+                format_print(transA, transB, headA, headB, "")
+                render.renderCount(transA, transB, str(headA), str(headB))
+    if renderList != []:
+        figIns = render.heatmapper(renderList)
+        figIns.rendermulti()
+        show()
 
 
 commands = 'combinedCompare', 'perProtein'
 
-@plac.annotations(input=('NamevID', 'positional'),
-                  same=('match', 'flag', 'm'))
-def combinedCompare(input: str, same: bool):
-    "Compare amino acids across all proteins in paired name/ID FASTAs"
-    return(compared(input, same))
 
-@plac.annotations(input=('NamevID', 'positional'),
+@plac.annotations(infile=('NamevID', 'positional'),
+                  same=('match', 'flag', 'm'))
+def combinedCompare(infile: str, same: bool):
+    "Compare amino acids across all proteins in paired name/ID FASTAs"
+    return(compared(infile, same))
+
+
+@plac.annotations(infile=('NamevID', 'positional'),
                   index=('protein index', 'positional'),
-                  all=('iterate through all', 'flag', 'a'),
+                  all_=('iterate through all', 'flag', 'a'),
                   residuals=('print protein net difference', 'flag', 'r'))
-def perProtein(input: str, index: int, all: bool, residuals: bool):
+def perProtein(infile: str, index: int, all_: bool, residuals: bool):
     "Compare amino acids for specific protein"
-    return(per(input, index, all, residuals))
+    return(per(infile, index, all_, residuals))
+
 
 def __missing__(name):
     return ('Unknown option: %r' % name)
+
 
 def __exit__(etype, exc, tb):
     "Will be called automatically at the end of the interpreter loop"
     if etype in (None, GeneratorExit):
         print('ok')
 
+
 main = __import__(__name__)
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     try:
         term_sized = get_terminal_size()
         term_size = term_sized.columns
-    except: 
-        term_size= 80
+    except ValueError:
+        term_size = 80
 
     plac.call(main)
--- src/render.py
+++ src/render.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 from matplotlib.pyplot import show
 from seaborn import catplot, pointplot, heatmap
 from pandas import DataFrame, concat
@@ -8,36 +7,45 @@ from matplotlib import pyplot, axes
 def renderCount(dict1: dict, dict2: dict, header1: str, header2: str):
     dataframe = DataFrame.from_records(data=[dict1, dict2])
     dataframe.insert(loc=0, column="Species", value=[header1, header2])
-    dataframe = dataframe.melt(id_vars=['Species'], var_name="Amino Acid", value_name="Residues")
-    catplot(kind="bar", data=dataframe, hue='Species', y="Residues", x="Amino Acid")
+    dataframe = dataframe.melt(
+        id_vars=['Species'], var_name="Amino Acid", value_name="Residues")
+    catplot(kind="bar", data=dataframe, hue='Species',
+            y="Residues", x="Amino Acid")
     show()
 
-def singleFrame(dict: dict, header: str):
-    dataframe = DataFrame.from_records([dict])
+
+def singleFrame(dicta: dict, header: str):
+    dataframe = DataFrame.from_records([dicta])
     dataframe.insert(loc=0, column="Species", value=[header])
     try:
         dataframe = dataframe.drop(columns=["GAP"])
     except KeyError:
         pass
     except BaseException:
+        print("fatal")
         raise
     dataframe = dataframe.melt(
         id_vars=['Species'], var_name="Amino Acid", value_name="Residues")
     return dataframe
 
-def renderSingle(dict: dict, header: str) -> axes.Axes:
-    dataframe = singleFrame(dict, header)
+
+def renderSingle(dicta: dict, header: str) -> axes.Axes:
+    dataframe = singleFrame(dicta, header)
     fig, ax = pyplot.subplots()
     pointplot(data=dataframe, hue='Species',
-          y="Residues", x="Amino Acid", ax=ax)
+              y="Residues", x="Amino Acid", ax=ax)
     return ax
+
 
 class heatmapper():
     renderTuple: list[tuple[dict, str]]
+
     def __init__(self, renderTup):
-        self.dfcol=concat([singleFrame(d, s) for d, s in renderTup])
+        self.dfcol = concat([singleFrame(d, s) for d, s in renderTup])
         self.figtmp, self.axitmp = pyplot.subplots(figsize=(9, 6))
-        self.evnum =self.figtmp.canvas.mpl_connect('button_press_event', self.onclick)
+        self.evnum = self.figtmp.canvas.mpl_connect(
+            'button_press_event', self.onclick)
+
     def rendermulti(self):
         combi = self.dfcol.pivot("Species", "Amino Acid", "Residues")
         heatmap(combi, ax=self.axitmp, yticklabels=False)
